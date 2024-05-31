388ada3ecd4e002af9c2d1a156c2d8a94e6b6b3a
Shreya <hum@ccf.org>
2024-05-31 14:47:37 -0400

    files

---
+++ Hub.Step2.R
@@ -0,0 +1,184 @@
+cellnames = "/home/mishras10/LC716/LC716_MAPQ40_cell_names_Shreya_light.txt"
+dir<-'/home/mishras10/LC716/chimeric.sc.pairs.mapq40/'
+metadata = '/home/mishras10/LC716/052124_LC716_MAPQ40_metadata_MultiContact.txt'
+annotation = '/home/hum/022221_local_genomic_features_Yunjiang/mm10/MboI/F_GC_M_MboI_10Kb_el.mm10.txt'
+color.loc = '/home/mishras10/LC716/color_20_YangOrder.txt'
+mm10.blacklist = '/home/mishras10/LC716/10KbBin_mm10_blacklist.txt'
+final.result = '/home/mishras10/LC716/hub_20celltypes_MAPQ40.txt'
+
+
+a <- read.table(cellnames, head=F)
+a<-as.data.frame(a)
+colnames(a)<-c('ID','CellType')
+dim(a)
+
+a$NumContact <- 0
+a$NumMultiContact <- 0
+a$Num10KbBin.with.MultiContact <- 0
+
+for(i in 1:nrow(a))
+{
+  if(i%%100==0)
+  {
+    print(c(i, i/nrow(a))) 
+  }
+  
+  u<-read.table(paste(dir, a$ID[i], '.', a$CellType[i],'.UU.NoSelfLigation.NoBlackList.pairs', sep=''), head=F)  
+  
+  z<-read.table(paste(dir, a$ID[i], '.', a$CellType[i], '.Noselfligation.NoBlackList.Multi10KbBin.UU.pairs', sep=''), head=F)
+  
+  a$NumContact[i] <- nrow(u)
+  a$NumMultiContact[i] <- nrow(z) 
+  
+  z.left <- z[,2:3]
+  z.right <- z[,4:5]
+  colnames(z.left)<-c('chr','pos')
+  colnames(z.right)<-c('chr','pos')
+  z.out <- unique( rbind(z.left, z.right) )
+  dim(z.out)
+  z.out <- z.out[order(z.out$chr, z.out$pos),]
+  
+  z.out$Bin <- floor(z.out$pos/1e4)*1e4
+  final<-unique(z.out[,-2])
+  dim(final) # 16,357 unique 10Kb bins
+  
+  a$Num10KbBin.with.MultiContact[i]<- nrow(final)
+  
+  write.table(final, file=paste(dir,'Bin.with.MultiContact.', a$ID[i], '.txt', sep=''),
+              row.names=F, col.names=T, sep='\t', quote=F)
+}
+write.table(a, file=metadata,row.names=F, col.names=T, sep='\t', quote=F)
+
+##########################################################################################################################################
+
+options(scipen=999)
+
+
+ann    <- read.table(annotation, sep='')
+ann <- as.data.frame(ann)
+colnames(ann)<- c('chr','start','end','F','GC','M')
+ann$ID <- paste(ann$chr, ann$start, sep='_')
+
+z<-read.table(metadata, head=T)
+p<-read.csv(color.loc, sep='\t', head=T)
+
+CELL20 <- as.data.frame( matrix(0, nrow=20, ncol=2) )
+colnames(CELL20)<-c('CellType','CellNum')
+CELL20[,1]<-p[,1]
+for(i in 1:20)
+{
+  CELL20[i,2] <- sum(z$CellType == CELL20[i,1])  
+}
+
+for(ID in 1:20)
+{
+  CELL.name <- CELL20[ID, 1]
+  CELL.num  <- CELL20[ID, 2]
+  
+  z.cell <- z[z$CellType == CELL.name,]
+  rec <- as.data.frame( matrix(0, nrow=nrow(ann), ncol=CELL.num) )
+  colnames(rec)<- z.cell$ID
+  
+  for(i in 1:nrow(z.cell))
+  {
+    x <- read.table(paste(dir,"/", 'Bin.with.MultiContact.',z.cell$ID[i],'.txt', sep=''), head=T)  
+    x$ID <- paste(x$chr, x$Bin, sep='_')
+    rec[,i]<- as.numeric( I(ann$ID %in% x$ID) )
+  }
+  
+  rec.sum <- apply(rec, 1, sum)
+  out <- cbind(ann[,1:6], rec.sum)
+  write.table(out, file=paste(dir,"/",'freq_',CELL.name,'.txt', sep=''),row.names=F, col.names=T, sep='\t', quote=F)
+}
+
+
+#####################################################################################################################################
+
+options(scipen=999)
+f<-function(KEY)
+{
+  a0<-read.table(paste(dir,"/",'freq_',KEY,'.txt', sep=''), head=T)
+  #dim(a0) # 272,566
+  
+  # keep autosomes
+  a1 <- a0[ a0$chr %in% paste('chr', 1:19, sep='')  ,] 
+  #dim(a1) # 246,285
+  
+  # keep 10Kb bins with mappability >0.8
+  a <- a1[a1$M>0.8,]
+  #dim(a) # 220,634
+  
+  a$ID <- paste(a$chr, a$start, sep='_')
+  
+  BLACK <- read.table(mm10.blacklist,head=T)
+  
+  #sum(a$ID %in% BLACK$ID)
+  
+  a<-a[!a$ID %in% BLACK$ID, ]
+  #dim(a) # 220,580 
+  
+  #barplot( table(a$rec.sum) )
+  
+  # trim top 1%
+  v <- sort(a$rec.sum)
+  v.cut <- v[ round(length(v)*0.99) ]
+  v.trim <- v[ v<=v.cut ]
+  
+  #barplot(table(v.trim))
+  #print(c(mean(v.trim), sd(v.trim)))
+  
+  v.mean <- mean(v.trim)
+  v.sd <- sd(v.trim)
+  
+  a$Zscore <- round((a$rec.sum - v.mean)/v.sd,4)
+  
+  out <- a[ a$Zscore > 1.96 ,]
+  #dim(out)
+  
+  print(c(KEY, nrow(out), nrow(out)/nrow(a)))
+  
+  write.table(out, file=paste(dir,"/",'hub_', KEY,'.txt', sep=''),
+              row.names=F, col.names=T, sep='\t', quote=F)
+}
+ann <- read.csv(color.loc, sep='\t', head=T)
+for(i in 1:20)
+{
+  f(ann[i,1])  
+}
+
+
+#######################################################################################################################
+
+ann <- read.csv(color.loc, sep='\t', head=T)
+
+x<-read.table(annotation, head=F)
+dim(x)
+x<-as.data.frame(x)
+colnames(x)<-c('chr','start','end','F','GC','M')
+x <- x[ x$chr %in% paste('chr', 1:19, sep='') & x$M>0.8 ,] 
+dim(x) # 220,634
+
+x$ID <- paste(x$chr, x$start, sep='_')
+BLACK <- read.table(mm10.blacklist, head=T)
+sum(x$ID %in% BLACK$ID)
+
+x<-x[!x$ID %in% BLACK$ID, ]
+dim(x) # 220,580
+
+options(scipen=999)
+
+rec<-as.data.frame(matrix(0,nrow=nrow(x),ncol=20))
+colnames(rec)<-ann[,1]
+
+for(i in 1:20)
+{
+  p<-read.table(paste(dir,"/",'hub_',ann[i,1],'.txt', sep=''),head=T)   
+  p$ID <- paste(p$chr, p$start, sep='_')
+  rec[,i] <- as.numeric(I(x$ID %in% p$ID))
+}
+
+final<-cbind(x, rec)
+write.table(final, file=final.result,
+            row.names=F, col.names=T, sep='\t', quote=F)
+
+
---
+++ Multiway.Contacts.Step1.R
@@ -0,0 +1,144 @@
+dir1 = "~/LC716/bam.sc/"
+dir2 = "~/LC716/chimeric.sc.pairs.mapq40/"
+mm10.chrom.size = "/home/mishras10/mm10.chrom.sizes"
+mm10.blacklist.region = "~/Droplet.FIREscore/mm10.blacklist.bed"
+
+files = list.files(path=dir1 ,pattern=".int.sam", all.files=FALSE, full.names=FALSE)
+
+for (file in files){
+  modified_string <- sub("\\.int.sam$", "", file)
+  command <- paste0("pairtools parse2 -c ",mm10.chrom.size," --assembly mm10 --add-pair-index --expand --drop-sam --min-mapq 40 ",dir1, file," -o ",dir2, modified_string,".pairs")
+  print(command)
+  system(command)
+  
+}
+
+
+files = list.files(path=dir2 ,pattern=".pairs", all.files=FALSE, full.names=FALSE)
+
+for (file in files){
+  
+  modified_string <- sub("\\.pairs$", "", file)
+  
+  command <- paste0("pairtools sort -o ",dir2,modified_string,"_sorted.pairs ",dir2, modified_string,".pairs")
+  print(command)
+  system(command)
+  
+  command <- paste0("pairtools dedup --mark-dups --output-stats ",dir2, modified_string,".dedup.stats -o ",dir2, modified_string,"_dedup.pairs ",dir2, modified_string,"_sorted.pairs")
+  print(command)
+  system(command)
+  
+  command <- paste0("pairtools select '(pair_type==\"UU\") and ((chrom1!=chrom2) or ((chrom1==chrom2) and (abs(pos1 - pos2) > 10000)))' ",dir2, modified_string,"_dedup.pairs > ",dir2, modified_string,".Noselfligation.UU.pairs")
+  print(command)
+  system(command)
+  
+}
+
+
+files = list.files(path=dir2, pattern=".Noselfligation.UU.pairs", all.files=FALSE, full.names=FALSE)
+for (file in files){
+  modified_string <- sub("\\.Noselfligation.UU.pairs$", "", file)
+  
+  a<-read.table(paste0(dir2, file), head=F)
+  a<-as.data.frame(a)
+  a$ID1 <- 0
+  a$ID2 <- 0
+  
+  x<-read.table(mm10.blacklist.region, head=F)
+  x<-as.data.frame(x)
+  colnames(x)<-c('chr','start','end')
+  nrow(x) # 164 blacklist
+  sum(x$end-x$start)/1e3 # 81.46Kb in total
+  x<-x[order(x$chr, x$start),]
+  
+  for(i in 1:nrow(x))
+  {
+    index1 <- I(a[,2]==x$chr[i] & a[,3]>=x$start[i] & a[,3]<=x$end[i])
+    index2 <- I(a[,4]==x$chr[i] & a[,5]>=x$start[i] & a[,5]<=x$end[i])
+    if(sum(index1)>=1)
+    {
+      a[index1==1, ]$ID1 <- 1
+    }
+    if(sum(index2)>=1)
+    {
+      a[index2==1, ]$ID2 <- 1
+    }
+  }
+  
+  table(a$ID1, a$ID2)
+  
+  # remove reads overlapping with mm10 blacklist region
+  y <- a[ a$ID1==0 & a$ID2==0 ,]
+  dim(y) # 92,452 contacts left
+  sum(y[,2]!=y[,4]) # 38,203 inter-chr contacts
+  sum(y[,2]==y[,4]) # 54,249 intra-chr contacts >10Kb
+  
+  write.table(y[,1:10], file=paste0(dir2, modified_string,".UU.NoSelfLigation.NoBlackList.pairs"), row.names=F, col.names=F, sep='\t', quote=F)
+  
+}
+
+
+files = list.files(path=dir2, pattern=".UU.NoSelfLigation.NoBlackList.pairs", all.files=FALSE, full.names=FALSE)
+for (file in files){
+  
+  modified_string <- sub("\\.UU.NoSelfLigation.NoBlackList.pairs$", "", file)
+  y<-read.table(paste0(dir2,file),head=F)
+  dim(y)
+  
+  nrow(y) # 57,983 rows
+  nrow(unique(y[,-1])) # 57,983
+  
+  PE <- table(y[,1])
+  length(PE) # 41,216 PE reads
+  
+  length(PE[PE==1]) # 32,349 PE reads only support pair-wise contacts
+  length(PE[PE>=2]) #  8,867 PE reads support multi-way contacts (>=3)
+  
+  u <- y[y[,1] %in% names(PE[PE==1]),] # pair-wise contacts
+  v <- y[y[,1] %in% names(PE[PE>=2]),] # multi-way contacts
+  
+  # summarize # of unique 10Kb bin for each PE read supporting multi-way contacts
+  rec <- as.data.frame(matrix(0, nrow=length(PE[PE>=2]), ncol=2))
+  colnames(rec)<-c('PE', 'Num.10Kb.Bin')
+  rec$PE <- names(PE[PE>=2])
+  
+  for(i in 1:nrow(rec))
+  {
+    if(i%%1000==0)
+    {
+      print(c(i, i/nrow(rec))) 
+    }
+    
+    w <- v[ v[,1]==rec$PE[i] ,]  
+    
+    w.l <- w[,2:3]
+    w.r <- w[,4:5]
+    colnames(w.l)<-c('chr','pos')
+    colnames(w.r)<-c('chr','pos')
+    anchor <- unique(rbind(w.l, w.r))
+    anchor <- anchor[order(anchor$chr, anchor$pos),]
+    anchor$Bin10Kb <- floor(anchor$pos/1e4)*1e4
+    anchor <- unique(anchor[,-2])
+    rec$Num.10Kb.Bin[i] <- nrow(anchor)
+  }
+  
+  table(rec$Num.10Kb.Bin)
+  
+  nrow(rec) # 8,867 PE reads supporting multi-way contacts (>=3)
+  out <- rec[ rec$Num.10Kb.Bin>=3 ,]
+  nrow(out) # 5,770 PE reads connecting 3 unique 10Kb bins
+  
+  table(rec[,2])
+  # 3,097 PE reads connect 2 unique 10Kb bins (need to remove)
+  # 5,017 PE reads connect 3 unique 10Kb bins 
+  #   651 PE reads connect 4 unique 10Kb bins
+  #    92 PE reads connect 5 unique 10Kb bins
+  #     9 PE reads connect 6 unique 10Kb bins
+  #     1 PE read  connect 7 unique 10Kb bins
+  
+  z<-y[y[,1] %in% out$PE,]
+  nrow(z)               # 19,245 contacts
+  length(unique(z[,1])) #  5,770 PE reads, each PE read connects >=3 unique 10Kb bins
+  
+  write.table(z, file=paste0(dir2, modified_string,'.Noselfligation.NoBlackList.Multi10KbBin.UU.pairs'),row.names=F, col.names=F, sep='\t', quote=F)
+}
